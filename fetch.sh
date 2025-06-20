9262f9e9064ae3d2a4aaf5a281e2742628b7f7e4
Máté Szabó <mszabo-oss@protonmail.com>
2025-06-20 17:46:40 +0200

    Fix automatic rebasing

--- fetch.sh
+++ fetch.sh
@@ -3,5 +3,7 @@ set -ex
 
 git clone -b introduce-cmake https://github.com/mszabo-wikia/mcrouter.git
 cd mcrouter
+git config user.name "mcrouter buildbot"
+git config user.email "build@example.com"
 git remote add upstream https://github.com/facebook/mcrouter.git
 git pull --rebase upstream main
